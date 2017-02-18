286d9c31adb51fe4f18bc1bdb225e8a850015acf
dangquocdung <dungthinhvn@gmail.com>
2017-02-18 21:53:47 +0700

    token

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -28,12 +28,12 @@ class QuyenHanTableSeeder extends Seeder
     {
       DB::table('quyenhan')->delete();
       DB::table('quyenhan')->insert([
-          ['quyenhan' => '1','ghichu' => 'Khách'],
-          ['quyenhan' => '3','ghichu' => 'Chuyên Viên'],
-          ['quyenhan' => '5','ghichu' => 'Lãnh Đạo Phòng'],
-          ['quyenhan' => '7','ghichu' => 'Văn Thư'],
-          ['quyenhan' => '9','ghichu' => 'Lãnh Đạo Đơn vị'],
-          ['quyenhan' => '15','ghichu' => 'Quản trị hệ thống']
+          ['quyenhan' => 'Khách','ghichu' => '#'],
+          ['quyenhan' => 'Chuyên Viên','ghichu' => 'chuyen-vien'],
+          ['quyenhan' => 'Trưởng Phòng','ghichu' => 'truong-phong'],
+          ['quyenhan' => 'Văn Thư','ghichu' => 'van-thu'],
+          ['quyenhan' => 'Lãnh Đạo','ghichu' => 'lanh-dao'],
+          ['quyenhan' => 'Quản trị','ghichu' => 'quantri']
       ]);
     }
 }
